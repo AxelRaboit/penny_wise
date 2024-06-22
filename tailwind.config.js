83f615b36bbec898add2ad5b782404c904946612
AxelRaboit <axel.raboit@gmail.com>
2024-06-22 21:41:29 +0200

    refactor: style for registration twig template

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,29 +8,29 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          DEFAULT: '#3b82f6', // Corresponds to bg-blue-500
-          hover: '#2563eb',   // Corresponds to hover:bg-blue-600
-          ring: '#3b82f6'     // Corresponds to focus:ring-blue-500
+          DEFAULT: '#9ca3af', // bg-gray-400
+          hover: '#6b7280',   // bg-gray-500
+          ring: '#9ca3af'     // bg-gray-400
         },
         secondary: {
-          DEFAULT: '#6b7280', // Corresponds to bg-gray-500
-          hover: '#4b5563',   // Corresponds to hover:bg-gray-600
-          ring: '#6b7280'     // Corresponds to focus:ring-gray-500
+          DEFAULT: '#6b7280', // bg-gray-500
+          hover: '#4b5563',   // hover:bg-gray-600
+          ring: '#6b7280'     // focus:ring-gray-500
         },
         success: {
-          DEFAULT: '#10b981', // Corresponds to bg-green-500
-          hover: '#059669',   // Corresponds to hover:bg-green-600
-          ring: '#10b981'     // Corresponds to focus:ring-green-500
+          DEFAULT: '#10b981', // bg-green-500
+          hover: '#059669',   // hover:bg-green-600
+          ring: '#10b981'     // focus:ring-green-500
         },
         danger: {
-          DEFAULT: '#ef4444', // Corresponds to bg-red-500
-          hover: '#dc2626',   // Corresponds to hover:bg-red-600
-          ring: '#ef4444'     // Corresponds to focus:ring-red-500
+          DEFAULT: '#ef4444', // bg-red-500
+          hover: '#dc2626',   // hover:bg-red-600
+          ring: '#ef4444'     // focus:ring-red-500
         },
         warning: {
-          DEFAULT: '#f59e0b', // Corresponds to bg-yellow-500
-          hover: '#d97706',   // Corresponds to hover:bg-yellow-600
-          ring: '#f59e0b'     // Corresponds to focus:ring-yellow-500
+          DEFAULT: '#f59e0b', // bg-yellow-500
+          hover: '#d97706',   // hover:bg-yellow-600
+          ring: '#f59e0b'     // focus:ring-yellow-500
         }
       }
     }
