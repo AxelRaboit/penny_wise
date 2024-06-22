3b8c58bc581b8e00fe579ae05d060edcb97ff38d
AxelRaboit <axel.raboit@gmail.com>
2024-06-22 17:34:14 +0200

    refactor: add logic about authenticated and unauthenticated users for RedirectAuthenticatedUserListener.php

--- src/EventListener/RedirectAuthenticatedUserListener.php
+++ src/EventListener/RedirectAuthenticatedUserListener.php
@@ -7,6 +7,7 @@ use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[AsEventListener(event: 'kernel.request', method: 'onKernelRequest')]
 final readonly class RedirectAuthenticatedUserListener
@@ -15,6 +16,8 @@ final readonly class RedirectAuthenticatedUserListener
     private const HOMEPAGE_ROUTE = 'app_homepage';
     private const REGISTER_ROUTE = 'app_register';
 
+    private const ROUTE = '_route';
+
     public function __construct(private Security $security, private RouterInterface $router){}
 
 
@@ -22,10 +25,26 @@ final readonly class RedirectAuthenticatedUserListener
     {
         $request = $event->getRequest();
         $user = $this->security->getUser();
+        $route = $request->attributes->get(self::ROUTE);
 
-        if ($user && in_array($request->attributes->get('_route'), [self::LOGIN_ROUTE, self::REGISTER_ROUTE])) {
+        if ($user instanceof UserInterface) {
+            $this->redirectAuthenticatedUser($event, $route);
+        } else {
+            $this->redirectUnauthenticatedUser($event, $route);
+        }
+    }
+
+    private function redirectAuthenticatedUser(RequestEvent $event, ?string $route): void
+    {
+        if (in_array($route, [self::LOGIN_ROUTE, self::REGISTER_ROUTE])) {
             $event->setResponse(new RedirectResponse($this->router->generate(self::HOMEPAGE_ROUTE)));
         }
     }
 
+    private function redirectUnauthenticatedUser(RequestEvent $event, ?string $route): void
+    {
+        if ($route === self::HOMEPAGE_ROUTE) {
+            $event->setResponse(new RedirectResponse($this->router->generate(self::LOGIN_ROUTE)));
+        }
+    }
 }
