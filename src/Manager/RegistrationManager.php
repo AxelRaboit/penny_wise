ce7801684f7d04c695f0ccc76ae1fdb3756362bf
AxelRaboit <axel.raboit@gmail.com>
2024-06-22 17:34:34 +0200

    feat: create Manager for Registration

---
+++ src/Manager/RegistrationManager.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Manager;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+final class RegistrationManager
+{
+    private const ROLE_USER = ['ROLE_USER'];
+    private const PASSWORD_FORM_FIELD = 'plainPassword';
+    public function __construct(private readonly UserPasswordHasherInterface $userPasswordHasher, private readonly EntityManagerInterface $entityManager){}
+    public function onUserRegistered(FormInterface $form, User $user): void
+    {
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user,
+                $form->get(self::PASSWORD_FORM_FIELD)->getData()
+            )
+        );
+        $user->setRoles(self::ROLE_USER);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+
+}
