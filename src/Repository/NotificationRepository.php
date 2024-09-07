20fd638d1b79dcce2801e6ffdd9cc948441d265c
AxelRaboit <axel.raboit@gmail.com>
2024-09-07 22:18:29 +0200

    feat: add Notification Entity

---
+++ src/Repository/NotificationRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Notification;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Order;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Notification>
+ */
+class NotificationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Notification::class);
+    }
+
+    public function getLastNthNotifications(int $nth): array
+    {
+        return $this->createQueryBuilder('n')
+            ->orderBy('n.id', Order::Descending->value)
+            ->setMaxResults($nth)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+//    /**
+//     * @return Notification[] Returns an array of Notification objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('n')
+//            ->andWhere('n.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('n.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Notification
+//    {
+//        return $this->createQueryBuilder('n')
+//            ->andWhere('n.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
