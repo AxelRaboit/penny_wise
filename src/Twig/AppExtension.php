58f61acba025a05e7b1c966d64c1b5a701df7f77
AxelRaboit <axel.raboit@gmail.com>
2024-09-07 23:14:56 +0200

    refactor: improve AppExtension::getMonthName by using MonthEnum and use it for the title of monthly template

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,8 +2,10 @@
 
 namespace App\Twig;
 
+use App\Enum\MonthEnum;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use ValueError;
 
 class AppExtension extends AbstractExtension
 {
@@ -14,23 +16,13 @@ class AppExtension extends AbstractExtension
         ];
     }
 
-    function getMonthName(int $monthNumber): string
+    public function getMonthName(int $monthNumber): string
     {
-        $months = [
-            1 => 'January',
-            2 => 'February',
-            3 => 'March',
-            4 => 'April',
-            5 => 'May',
-            6 => 'June',
-            7 => 'July',
-            8 => 'August',
-            9 => 'September',
-            10 => 'October',
-            11 => 'November',
-            12 => 'December',
-        ];
-
-        return $months[$monthNumber] ?? 'Invalid month';
+        try {
+            $monthEnum = MonthEnum::from($monthNumber);
+            return $monthEnum->getName();
+        } catch (ValueError $e) {
+            return `Invalid month number: ${e->getMessage()}`;
+        }
     }
 }
