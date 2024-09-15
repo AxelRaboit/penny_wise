f184649683dc18a0ceee6c15d371153058151be5
AxelRaboit <axel.raboit@gmail.com>
2024-09-15 23:57:20 +0200

    feat: add listener to check the validity of the wallet form

--- src/Form/WalletType.php
+++ src/Form/WalletType.php
@@ -7,6 +7,7 @@ namespace App\Form;
 use App\Entity\Wallet;
 use App\Enum\CurrencyEnum;
 use App\Enum\MonthEnum;
+use App\EventListener\WalletListener;
 use Override;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -14,10 +15,14 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class WalletType extends AbstractType
 {
+    public function __construct(private readonly WalletListener $walletListener) {}
+
     #[Override]
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -25,14 +30,14 @@ class WalletType extends AbstractType
             ->add('year', IntegerType::class, [
                 'attr' => ['placeholder' => 'Choose a year'],
             ])
-            ->add('month', ChoiceType::class, [
+            /*->add('month', ChoiceType::class, [
                 'choices' => array_combine(
                     array_map(fn (MonthEnum $month): string => $month->getName(), MonthEnum::all()),
                     MonthEnum::all()
                 ),
                 'placeholder' => 'Choose a month',
                 'choice_value' => fn (?MonthEnum $month) => $month?->value,
-            ])
+            ])*/
             ->add('start_date', DateType::class, [
                 'widget' => 'single_text',
             ])
@@ -54,6 +59,9 @@ class WalletType extends AbstractType
                 'attr' => ['placeholder' => 'Choose a spending limit'],
                 'required' => false,
             ])
+            ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event): void {
+                $this->walletListener->onPostSubmit($event);
+            });
         ;
     }
 
