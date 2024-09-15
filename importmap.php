22ce35ae953f9df1da02af10584016bd354d50ea
AxelRaboit <axel.raboit@gmail.com>
2024-09-15 23:58:33 +0200

    feat: install symfony/ux-autocomplete

--- importmap.php
+++ importmap.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Returns the importmap for this application.
  *
@@ -52,5 +50,8 @@ return [
     ],
     '@symfony/ux-live-component' => [
         'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
+    ],
+    'tom-select' => [
+        'version' => '2.3.1',
     ],
 ];
