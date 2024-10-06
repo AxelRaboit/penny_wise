9f6ed3b209df5a5aed8a81d37018044d6f4cdcfc
AxelRaboit <axel.raboit@gmail.com>
2024-10-06 16:04:56 +0200

    fix: account list delete year button

--- assets/js/page/accountList/accountList.js
+++ assets/js/page/accountList/accountList.js
@@ -13,6 +13,17 @@ export function initializeWalletListModals() {
     });
 
     attachModalEvents({
+        modalId: 'deleteYearModal',
+        modalTitle: 'Delete year and all of it\'s months and transactions',
+        modalMessage: 'Are you sure you want to delete this year?',
+        triggerButtonSelector: '.js-delete-year-button',
+        confirmButtonId: 'confirmDeleteWallet',
+        confirmLabel: 'Delete',
+        cancelButtonId: 'cancelDeleteYear',
+        cancelLabel: 'Cancel',
+    });
+
+    attachModalEvents({
         modalId: 'deleteAccountModal',
         modalTitle: 'Delete account',
         modalMessage: 'Are you sure you want to delete the account?',
