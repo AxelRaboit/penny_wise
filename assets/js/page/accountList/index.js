e04f36b6440a7926ca92547cc40607b7ca8b9505
AxelRaboit <axel.raboit@gmail.com>
2024-10-12 12:40:06 +0200

    feat: spinners

--- assets/js/page/accountList/index.js
+++ assets/js/page/accountList/index.js
@@ -1,5 +1,7 @@
 import {initializeWalletListModals} from "./account_list.js";
+import {handlePageSpinner} from "../../component/Spinner/page-spinner.js";
 
 document.addEventListener('DOMContentLoaded', function () {
+    handlePageSpinner('account-list-page', 'loadingPageSpinner');
     initializeWalletListModals();
 });
--- assets/js/page/monthly/index.js
+++ assets/js/page/monthly/index.js
@@ -1,8 +1,10 @@
 import {initializeChartButtons} from "./chart.js";
 import {initializeMonthlyModals} from "./monthly.js";
 import {initializeMonthlySideMenu} from "./side_menu.js";
+import {handlePageSpinner} from "../../component/Spinner/page-spinner.js";
 
 document.addEventListener('DOMContentLoaded', function () {
+    handlePageSpinner('wallet-dashboard-page', 'loadingPageSpinner');
     initializeChartButtons();
     initializeMonthlyModals();
     initializeMonthlySideMenu();
