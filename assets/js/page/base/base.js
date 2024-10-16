0abcc76b7d3c934e50465021fdb3eaa92fab4646
AxelRaboit <axel.raboit@gmail.com>
2024-10-16 20:56:23 +0200

    feat: loading on body

--- assets/js/page/base/base.js
+++ assets/js/page/base/base.js
@@ -1,8 +1,10 @@
 import { initializeClock } from './component/clock.js';
 import {initializeAddFlash} from './component/addFlash.js';
 import {initializeSideMenu} from './component/sideMenu.js';
+import {handlePageSpinner} from "../../component/Spinner/page-spinner.js";
 
 document.addEventListener('DOMContentLoaded', function() {
+    handlePageSpinner('app-page', 'loadingPageSpinner');
     initializeSideMenu();
     initializeAddFlash();
     initializeClock();
