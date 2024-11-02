0411f36dba8b824924d3e68b77adb2094d55a87d
AxelRaboit <axel.raboit@gmail.com>
2024-11-02 18:22:37 +0100

    fix

--- assets/js/page/base/base_no_topbar.js
+++ assets/js/page/base/base_no_topbar.js
@@ -1,5 +1,7 @@
 import {handlePageSpinner} from "@component/spinner/page_spinner.js";
+import {initializeThemeSwitcher} from "@page/base/component/theme_switcher";
 
 document.addEventListener('DOMContentLoaded', function() {
     handlePageSpinner('base-no-topbar-page', 'loadingPageSpinner');
+    initializeThemeSwitcher();
 });
